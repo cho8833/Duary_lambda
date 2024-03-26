5910ab574f2e82fb7d52c60dc953702047ff9602
hyeonbin-charlie <hyeonbin@pathfinder.camp>
2024-03-26 10:02:34 +0900

    fixed pointer

--- cmd/user/get_user.go
+++ cmd/user/get_user.go
@@ -4,12 +4,16 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/cho8833/CC-Calendar/config"
 	"github.com/cho8833/CC-Calendar/internal/user/api"
+	"github.com/cho8833/CC-Calendar/internal/user/repository"
 	"github.com/cho8833/CC-Calendar/internal/user/repository/dynamodb"
+	"github.com/cho8833/CC-Calendar/internal/user/service"
 	"github.com/cho8833/CC-Calendar/internal/user/service/impl"
 )
 
 func main() {
 	option, _ := config.GetDynamoDBOption()
-	userAPI := api.NewUserAPI(impl.NewUserService(dynamodb.NewUserRepository(option.Client)))
+	var userRepository repository.UserRepository = dynamodb.NewUserRepository(option.Client)
+	var userService service.UserService = impl.NewUserService(&userRepository)
+	userAPI := api.NewUserAPI(&userService)
 	lambda.Start(userAPI.GetUser)
 }
