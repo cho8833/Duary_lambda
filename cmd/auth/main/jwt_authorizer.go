31047f94f3056431e3a36b47a4b8c6ad49e90bfc
cho8833 <chowsp1009@gmail.com>
2024-09-23 12:59:45 +0900

    fixed jwt authorizer

--- cmd/auth/main/jwt_authorizer.go
+++ cmd/auth/main/jwt_authorizer.go
@@ -7,21 +7,34 @@ import (
 	"github.com/cho8833/duary_lambda/internal/auth/jwtutil"
 	"log"
 	"os"
+	"strings"
 )
 
-func jwtAuthorizer(ctx context.Context, request events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayV2CustomAuthorizerIAMPolicyResponse, error) {
-	if request.Type != "Bearer" {
-		return denyResponse(), nil
-	}
+/*
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -trimpath -tags lambda.norpc -o bootstrap cmd/auth/main/jwt_authorizer.go && chmod 755 bootstrap && zip  build/package/jwt_authorizer.zip bootstrap && rm bootstrap
+*/
+func jwtAuthorizer(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayV2CustomAuthorizerIAMPolicyResponse, error) {
 	key := os.Getenv("secretKey")
-	jwtUtil := jwtutil.JWTUtilImpl{}
-	id, err := jwtUtil.ValidateApplicationJWT(request.AuthorizationToken, key)
+	jwtUtil := jwtutil.Impl{}
+	header := request.Headers["authorization"]
+	log.Printf("got authorization : %s", header)
+
+	check := strings.Split(header, " ")
+	token := check[1]
+	id, err := jwtUtil.ValidateApplicationJWT(token, key)
 	if err != nil {
-		log.Printf("Authorization: fail to authorize. token: %s, error: %s", request.AuthorizationToken, err.Error())
+		log.Printf("Authorization: fail to authorize. token: %s, error: %s", token, err.Error())
 		return denyResponse(), nil
 	}
+	s := strings.Split(*id, "-")
+
+	log.Printf("authorized %s", *id)
 	return events.APIGatewayV2CustomAuthorizerIAMPolicyResponse{
 		PrincipalID: *id,
+		Context: map[string]interface{}{
+			"socialId": s[0],
+			"provider": s[1],
+		},
 		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
 			Version: "2012-10-17",
 			Statement: []events.IAMPolicyStatement{
@@ -29,7 +42,7 @@ func jwtAuthorizer(ctx context.Context, request events.APIGatewayCustomAuthorize
 					Action: []string{"execute-api:Invoke"},
 					Effect: "Allow",
 					Resource: []string{
-						"arn:aws:apigateway:ap-northeast-2::/apis/i3lm91q9v5/*",
+						"arn:aws:execute-api:ap-northeast-2:922001515124:i3lm91q9v5/*",
 					},
 				},
 			},
